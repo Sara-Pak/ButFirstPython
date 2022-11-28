0ab9ccc7530a3e69725bd1c7c69f0ebaf0a9ddbc
Sara-pak <sarabpak@gmail.com>
2022-11-27 22:58:56 -0500

    refactor algorithm

--- BMIcalulator.py
+++ BMIcalulator.py
@@ -6,3 +6,9 @@ weight = input("Enter your weight in kg: ")
 
 result = float(weight) / (float(height) * float(height))
 print(int(result))
+
+#alt solution
+
+result = int(weight)/float(height) ** 2
+result_as_whole_number = int(result)
+print(result_as_whole_number)
