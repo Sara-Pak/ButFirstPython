32c34a86b896a1d77f87d28b402adbb2a1d28b9a
Sara-pak <sarabpak@gmail.com>
2022-12-11 23:42:11 -0500

    add logic to break the inifinite loop.

--- main.py
+++ main.py
@@ -101,7 +101,8 @@ def print_range(start, end):
     # loop through the numbers from start to end
     n = start
     while n <= end:
-        print(n)
+        n += 1
+        print(n - 1)
 
 
 print_range(1, 5)  # should print 1 2 3 4 5 (each number on its own line)
