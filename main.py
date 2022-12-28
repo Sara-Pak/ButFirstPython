54dd06d980807ebd2c41df428ed3d1e4b6cfbc69
Sara-pak <sarabpak@gmail.com>
2022-12-27 23:57:10 -0500

    add for recursion examples/structure

--- main.py
+++ main.py
@@ -177,3 +177,25 @@ def greet_friends(friends):
 
 greet_friends(['taylor', 'lois', 'kenny'])
 
+
+# recursion structure example
+
+# def recursive_function(parameters):
+#   if base_case_condition(parameters):
+#      return base_case_value
+# recursive_function(modified_parameters)
+
+
+def is_power_of(number, base):
+    # Base case: when number is smaller than base.
+    if number < base:
+        # If number is equal to 1, it's a power (base**0).
+        return base, number
+
+    # Recursive case: keep dividing number by base.
+    return is_power_of(base, number)
+
+
+print(is_power_of(8, 2))  # Should be True
+print(is_power_of(64, 4))  # Should be True
+print(is_power_of(70, 10))  # Should be False
